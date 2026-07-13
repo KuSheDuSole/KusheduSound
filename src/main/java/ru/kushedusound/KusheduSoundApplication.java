b52e052de2fbd0223c9daa4b2b857221abe4dce8
KuSheDuSole <kefirka552@gmail.com>
2026-07-13 18:39:48 +0300

    First commit

---
+++ src/main/java/ru/kushedusound/KusheduSoundApplication.java
@@ -0,0 +1,13 @@
+package ru.kushedusound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusheduSoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KusheduSoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kushedusound/KusheduSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kushedusound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusheduSoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
