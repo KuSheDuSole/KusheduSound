56609aec0598a589fd875c6dfe6a6ac94110553c
KuSheDuSole <kefirka552@gmail.com>
2026-08-05 21:48:36 +0300

    Add validation to UserCreateRequestDto

--- src/main/java/ru/kushedusound/entity/dto/request/create/UserCreateRequestDto.java
+++ src/main/java/ru/kushedusound/entity/dto/request/create/UserCreateRequestDto.java
@@ -1,8 +1,12 @@
 package ru.kushedusound.entity.dto.request.create;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserCreateRequestDto(
-        String username,
-        String email,
-        String password
+        @NotBlank @Size(min = 3, max = 30) String username,
+        @NotBlank @Email String email,
+        @NotBlank @Size(min = 12, max = 72) String password
 ) {
 }
